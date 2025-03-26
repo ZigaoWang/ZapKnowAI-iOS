5a2c268af548c4823c545e080321f978ea9c8099
July Wu <toothless2012.pp@gmail.com>
2025-03-26 15:45:37 +0800

    Intialize xcode project

---
+++ ZhidaoAI/ZhidaoAI/ZhidaoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhidaoAIApp.swift
+//  ZhidaoAI
+//
+//  Created by July WU on 2025/3/12.
+//
+
+import SwiftUI
+
+@main
+struct ZhidaoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZhidaoAI/ZhidaoAITests/ZhidaoAITests.swift
@@ -0,0 +1,16 @@
+//
+//  ZhidaoAITests.swift
+//  ZhidaoAITests
+//
+//  Created by July WU on 2025/3/12.
+//
+
+import Testing
+
+struct ZhidaoAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
