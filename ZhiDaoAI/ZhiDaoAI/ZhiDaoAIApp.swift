2f177c2b611a763b1a7a527033e6d7269a56c6a3
Zigao Wang <zigao.wang24@gmail.com>
2025-03-26 15:57:41 +0800

    feat: empty ios project

---
+++ ZhiDaoAI/ZhiDaoAI/ZhiDaoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhiDaoAIApp.swift
+//  ZhiDaoAI
+//
+//  Created by Zigao Wang on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ZhiDaoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
